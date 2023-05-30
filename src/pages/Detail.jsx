cfca9f3ca9b0ba4c8f991fa23afafd90642955b8
ryan3780 <guemhong3780@gmail.com>
2023-05-30 12:14:06 +0900

    Add useQuery in Detail.jsx

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,40 @@
-
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
 
 export const Detail = () => {
 
+  const { id } = useParams()
+
+  const getDetailProduct = async () => {
+    const { data } = await axios.get(`http://localhost:5001/products/${id}`);
+    console.log(data)
+    return data;
+  }
+
+  const { isLoading, isError, data } = useQuery(['Detail_Prodcut'], getDetailProduct);
+
+  if (isLoading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div>Error...</div>
+    )
+  }
+
+
   return (
     <>
-      Detail
+
+      <div key={data.id}>
+        <img src={data.image} alt="proudct image" width={`300px`} />
+
+      </div>
+
     </>
   )
 }
