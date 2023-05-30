41ff0418f8882591b5fb1b10aa2d7681d6330424
ryan3780 <guemhong3780@gmail.com>
2023-05-30 12:05:47 +0900

    Add useQuery in Products.jsx & Change json-server port from 5000 => 5001

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,40 @@
-
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 export const Products = () => {
 
+
+  const getProducts = async () => {
+    const { data } = await axios.get('http://localhost:5001/products');
+    console.log(data)
+    return data;
+  }
+
+  const { isLoading, isError, data } = useQuery(['All_Prodcuts'], getProducts);
+
+  if (isLoading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div>Error...</div>
+    )
+  }
+
+
   return (
     <>
-      Products
+      {data.map((product, idx) => {
+        return (
+          <div key={idx}>
+            <img src={product.image} alt="product image" width={`300px`} />
+            {product.category}
+          </div>
+        )
+      })}
     </>
   )
 
