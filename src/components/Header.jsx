264a07cfd02e446b016c080bdc88093551c598ed
ryan3780 <guemhong3780@gmail.com>
2023-05-30 11:59:01 +0900

    Add skeleton header contents to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,23 @@
 
 export const Header = () => {
 
+  const headerContents = [
+    'Main',
+    'ALL Products',
+    'Cart'
+  ]
+
   return (
 
-    <div>
-      Header
-    </div>
+    <>
+      {headerContents.map((item, idx) => {
+        return (
+          <li key={idx}>
+            {item}
+          </li>
+        )
+      })}
+    </>
 
   )
 }
